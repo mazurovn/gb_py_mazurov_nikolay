6490925720658d7057d3dbe3d44bb8ee3dd259ad
mazurovn <94653181+mazurovn@users.noreply.github.com>
2021-12-04 20:02:18 +0300

    add DZ 4  Nmaz
    add DZ 4

---
+++ Mazurov_Nikolay_DZ_4/DZ_4_5_NMaz_start.py
@@ -0,0 +1,8 @@
+# Доработать скрипт из предыдущего задания:
+# теперь он должен работать и из консоли.
+import sys
+import DZ_4_5_NMaz_def
+
+inuse = sys.argv[1]
+print(DZ_4_5_NMaz_def.currency_rate(inuse))
+
